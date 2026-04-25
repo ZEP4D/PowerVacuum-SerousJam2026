3d6ac258021172105a660c066f8cbff14678025c
ZEP4D <bkropiewnicki23@gmail.com>
2026-04-25 17:33:59 +0200

    006

--- Assets/Scripts/EndChecker.cs
+++ Assets/Scripts/EndChecker.cs
@@ -3,33 +3,47 @@ using UnityEngine;
 public class EndChecker : MonoBehaviour
 {
     [SerializeField] private int howmanyturn;
+    private bool endisactive;
+
+
+    void Start()
+    {
+        endisactive = false;
+    }
+
 
     void Update()
     {
-        if(ResourcesSystem.instance.getApproval() < 0)
-        {
-            Invoke(nameof(YouLose),0);    
-        }
-        else if(ResourcesSystem.instance.getbudget() < 0)
-        { 
-            Invoke(nameof(YouLose),0);
-        }
-        else if(ResourcesSystem.instance.getclimate() > 100)
-        {
-            Invoke(nameof(YouLose),0);
-        }
 
-        if(ResourcesSystem.instance.getnumbersofturn() > howmanyturn)
+
+        if (!endisactive)
         {
-            if(ResourcesSystem.instance.getApproval() < 50)
+            if(ResourcesSystem.instance.getApproval() < 0)
+            {
+                Invoke(nameof(YouLose),0);    
+            }
+            else if(ResourcesSystem.instance.getbudget() < 0)
+            { 
+                Invoke(nameof(YouLose),0);
+            }
+            else if(ResourcesSystem.instance.getclimate() > 100)
             {
                 Invoke(nameof(YouLose),0);
             }
-            else
+
+            if(ResourcesSystem.instance.getnumbersofturn() > howmanyturn)
             {
-                Invoke(nameof(Youwin),0);
+                if(ResourcesSystem.instance.getApproval() < 50)
+                {
+                    Invoke(nameof(YouLose),0);
+                }
+                else
+                {
+                    Invoke(nameof(Youwin),0);
+                }
             }
         }
+       
     }
 
 
@@ -37,12 +51,14 @@ public class EndChecker : MonoBehaviour
     {   
         Debug.Log("Lost");
         Debug.Log("hehehehehe");
+        endisactive = true;
     }
 
     void Youwin()
     {
         Debug.Log("Win");
         Debug.Log("Sad face,Sad Face");
+        endisactive = true;
     }
 
 
