07a15074697e2c4ccc9dc242eafab54bc4c69679
ZEP4D <bkropiewnicki23@gmail.com>
2026-04-26 12:31:43 +0200

    010

--- Assets/Scripts/Foreign_relations.cs
+++ Assets/Scripts/Foreign_relations.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 public class Foreign_relations : MonoBehaviour
 {
     
-    [SerializeField] private List<Country_core> Countries; 
+    [SerializeField] private List<Country_core> countries; 
+
+    
+    public int getrelation(Country_core core)
+    {
+        int index = Listfinder(core);
+        return countries[index].getrelation();
+    }
+
+
+    public int getliked(Country_core core)
+    {
+        int index = Listfinder(core);
+        return countries[index].getliked();
+    }
+
+
 
     
 
 
+
+    private int Listfinder(Country_core count)
+    {
+        for(int i = 0; i < countries.Count; i++)
+        {
+            if(countries[i] == count)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
 
 }
