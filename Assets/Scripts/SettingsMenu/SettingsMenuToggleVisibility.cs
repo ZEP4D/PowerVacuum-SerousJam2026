1c12e3465f7618fc2a45b335b14a4586d6d7d54c
ThePolishTech <162223303+ThePolishTech@users.noreply.github.com>
2026-05-29 22:12:44 +0200

    Made canvas toggle on escape

---
+++ Assets/Scripts/SettingsMenu/SettingsMenuToggleVisibility.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SettingsMenuToggleVisibility : MonoBehaviour
+{
+    [SerializeField] Canvas settings_menu_canvas;
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Escape) ) {
+            settings_menu_canvas.enabled = !settings_menu_canvas.enabled;
+        }
+    }
+}
