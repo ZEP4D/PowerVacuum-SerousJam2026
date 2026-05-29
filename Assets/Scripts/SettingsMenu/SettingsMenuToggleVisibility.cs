1613c18352cfc446fc532da70e23ddaae13c2da0
ThePolishTech <162223303+ThePolishTech@users.noreply.github.com>
2026-05-29 22:16:02 +0200

    Sets the canvas to not destroy on load

--- Assets/Scripts/SettingsMenu/SettingsMenuToggleVisibility.cs
+++ Assets/Scripts/SettingsMenu/SettingsMenuToggleVisibility.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class SettingsMenuToggleVisibility : MonoBehaviour
 {
     [SerializeField] Canvas settings_menu_canvas;
+    
     void Update()
     {
         if ( Input.GetKeyDown(KeyCode.Escape) ) {
             settings_menu_canvas.enabled = !settings_menu_canvas.enabled;
         }
+    }
+
+    void Start() {
+        DontDestroyOnLoad(this.gameObject);
     }
 }
