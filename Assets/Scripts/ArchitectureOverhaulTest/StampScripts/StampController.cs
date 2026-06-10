3815385d41295143b583d1a5ecb2feb9e4d31636
ThePolishTech <162223303+ThePolishTech@users.noreply.github.com>
2026-06-10 19:51:34 +0200

    StampController detects when hovering over a stamp slot; some more script files created

--- Assets/Scripts/ArchitectureOverhaulTest/StampScripts/StampController.cs
+++ Assets/Scripts/ArchitectureOverhaulTest/StampScripts/StampController.cs
@@ -1,21 +1,102 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class StampController : MonoBehaviour
+using System;
+using System.Text;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class StampController : MonoBehaviour, IPointerClickHandler
 {
     // --== SERIALIZED FIELDS ==-- //
         [SerializeField] StampSlotController homeSlot;
         [SerializeField] StampType stampType;
     // ==--
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // --== ATTACHED COMPONENTS ==-- //
+        private SpriteRenderer spriteRenderer;
+        private BoxCollider2D  boxCollider2D; 
+    // ==--
+
+
+    // --== CLASS FIELDS ==-- //
+        private StampState currentStampState = StampState.Idle;
+    // ==--
+
+
+    // --== CLASS METHODS ==-- //
+        private void HandleLeftClick()
+        {
+            switch (this.currentStampState)
+            {
+                case StampState.Idle:
+                    this.currentStampState = StampState.Held;
+                break;
+
+                case StampState.Held:
+                break;
+
+                case StampState.MovingToArea:
+                case StampState.Placed:
+                break;
+            }
+        }
+    // ==--
+
+
+    // --== 'IPointerClickHandler' METHODS ==-- //
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (pointerEventData.button == PointerEventData.InputButton.Left)
+            {
+                this.HandleLeftClick();
+            }
+        }
+    // ==--
+
+
+    // --== UNITY METHODS ==-- //
+        void Start()
+        {
+            this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+            this.boxCollider2D  = this.GetComponent<BoxCollider2D>();
+        }
+
+        void Update() 
+        { 
+            switch (this.currentStampState)
+            {
+                case StampState.Held:
+                    var mousePosition = Mouse.current.position.ReadValue();
+                    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                    this.transform.position = mousePosition;
+
+                    foreach (
+                        BoxCollider2D candidateCollider
+                        in FindObjectsByType<BoxCollider2D>(FindObjectsSortMode.None)
+                    ) {
+                        if (
+                            candidateCollider == this.boxCollider2D
+                        ) continue;
+
+                        if ( 
+                            this.boxCollider2D.IsTouching(candidateCollider)
+                            & candidateCollider.gameObject.GetComponent<StampSpaceController>() != null
+                        ) {
+                            Debug.Log("Stamp is touching: " + candidateCollider.name);
+                        }
+                    }
+                break;
+
+                case StampState.MovingToArea:
+                case StampState.Idle:
+                case StampState.Placed:
+                break;
+            }
+        } // void Update()
+
+    // ==--
 }
---
+++ Assets/Scripts/ArchitectureOverhaulTest/StampScripts/StampSlotState.cs
@@ -0,0 +1,5 @@
+public enum StampSlotState
+{
+    Occupied,
+    Empty,
+}
