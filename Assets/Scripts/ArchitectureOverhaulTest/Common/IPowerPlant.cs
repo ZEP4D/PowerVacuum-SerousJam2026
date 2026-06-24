56db0a3c1d6a23d5ff8c3d36b6a53017dfd584d0
ThePolishTech <162223303+ThePolishTech@users.noreply.github.com>
2026-06-24 19:59:23 +0200

    further scene setup

---
+++ Assets/Scripts/ArchitectureOverhaulTest/Common/IPowerPlant.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ArchitectureOverhaul
+{
+    public class IPowerPlant
+    {
+    
+    }
+}
--- Assets/Scripts/ArchitectureOverhaulTest/Controllers/MasterControlProgram.cs
+++ Assets/Scripts/ArchitectureOverhaulTest/Controllers/MasterControlProgram.cs
@@ -10,12 +10,26 @@ namespace ArchitectureOverhaul
     public class MasterControlProgram : MonoBehaviour
     {
         // --== SERIALIZED FIELDS ==-- //
-            [SerializeReference] List<DayController> m_dayControllers;
-            [SerializeField] IStampInteractable m_stampArea;
+            [SerializeField] List<DayController> m_dayControllers;
+            [SerializeField] StampAreaController m_stampArea;
+
+            [SerializeField] PageArrowController m_leftArrow;
+            [SerializeField] PageArrowController m_rightArrow;
+            [SerializeField] EndDayButtonController m_endDayButton;
+
+            [SerializeField] DocumentController m_document;
         // ==--
 
 
         // --== CLASS FIELDS ==-- //
+            private List< KeyValuePair<DayController, List<DocumentData>> > m_loadedDays;
+            private uint m_currentDay = 0;
+            private uint m_currentDocument = 0;
+
+            /// <summary>
+            /// The key of this dictionary is the type of a class implementing IPowerPlant
+            /// </summary>
+            private Dictionary<Type, List<IPowerPlant>> m_powerPlants;
         // ==--
 
 
@@ -28,6 +42,10 @@ namespace ArchitectureOverhaul
 
 
         // --== UNITY METHODS ==-- //
+            void Start()
+            {
+                //
+            }
         // ==--
     }
 }
